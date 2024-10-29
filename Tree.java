f4ba71d53c111d13ae7c33f25c7f495faca36069
Md. Golam Kibria <78980142+kibria1010@users.noreply.github.com>
2024-10-29 11:42:41 +0600

    Add files via upload

--- Tree.java
+++ Tree.java
@@ -1,5 +1,12 @@
 package oopconcepts;
 
+/**
+ * A TreeMap is an implementation of the Map interface, where its items are stored as key-value pairs and it is sorted either by natural ordering of its keys or through a Comparator.
+ * A TreeMap differs from a HashMap in terms of how their contents are stored. A HashMap stores its keys as hashes in a hash table whereas a TreeMap stores its keys in a binary tree structure, which is a type of tree data structure that has at most two children nodes per parent.
+ * While the structure of a TreeMap is more orderly, it comes as a tradeoff with speed in comparison to a HashMap. In Big-O notation, most HashMap operations run in amortized O(1) time; they are generally more time-efficient. Most TreeMap operations run in O(log(n)) time and the TreeMap is generally more space-efficient due to its dynamic allocation of memory per value added.
+ * Note: Complexity is a crucial factor when determining what structure to implement. For faster operations over data, aim for a HashMap. For structured and sorted operations on data, it’s best to stick to a TreeMap.
+ */
+
 import java.util.*;
 
 public class Tree {
